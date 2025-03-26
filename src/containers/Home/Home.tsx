15b3a495b087ed423bd73b5b836290d8b8292950
alberto-2-garcia <alberto.2.garcia@eviden.com>
2025-03-26 16:30:49 -0600

    Added home page

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,14 +1,25 @@
-import { Box, Container } from '@mui/material';
+import { Box, Container, Grid2 as Grid } from '@mui/material';
 import Navbar from '../../components/Navbar/Navbar';
 import { PageTitle } from '../../components/StyledComponents/StyledComponents'
+import ProductCard from '../Products/ProductCard/ProductCard';
+import useProducts from '../../hooks/useProducts/useProducts';
 
 
 function Home() {
+    const { products } = useProducts({ productId: '' });
+
     return (
         <Box>
-        <Navbar />
+            <Navbar />
             <Container maxWidth='xl'>
                 <PageTitle variant='h4'>Home</PageTitle>
+                <Grid container spacing={2} alignItems="stretch">
+                    {products?.map((product) => (
+                        <Grid key={product.id} size={2}>
+                            <ProductCard {...product} />
+                        </Grid>
+                    ))}
+                </Grid>
             </Container>
         </Box>
     )
