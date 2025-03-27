66f4e212061c7b15f6dd4c2d8197d5ee2826c284
alberto-2-garcia <alberto.2.garcia@eviden.com>
2025-03-27 15:09:37 -0600

    Started e2e testing with cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,12 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
+
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+        expect(true).to.equal(true)
+    });
+    it('Does not do much!', () => {
+        expect(true).to.equal(false)
+    })
+})
